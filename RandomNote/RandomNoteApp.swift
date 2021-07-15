624ca859ffea2d571c4f61578da18276856780a5
gentj <Jamie.Gentry@pega.com>
2021-07-15 13:38:32 -0400

    Initial Commit

---
+++ RandomNote/RandomNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNoteApp.swift
+//  RandomNote
+//
+//  Created by Gentry, Jamie on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
